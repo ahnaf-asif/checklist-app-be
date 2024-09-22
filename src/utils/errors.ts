731f7b10681c0d966100f8307617129d2b89b7b2
Mubasshir Chowdhury <md.mubasshir.chowdhury@gmail.com>
2024-09-23 05:15:01 +0600

    Add NoRecordFoundError and InvalidFieldError

---
+++ src/utils/errors.ts
@@ -0,0 +1,33 @@
+export class NoRecordFoundError extends Error {
+  public readonly status: number;
+  constructor(
+    public readonly tableName: string,
+    public readonly id: number
+  ) {
+    super('No record found');
+    this.name = 'NoRecordFoundError';
+    this.message = `No record found for table: '${tableName}' with id: '${id}'`;
+    this.status = 404;
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, NoRecordFoundError);
+    }
+  }
+}
+
+export class InvalidFieldError extends Error {
+  public readonly status: number;
+  constructor(
+    public readonly tableName: string,
+    public readonly field: string
+  ) {
+    super('Invalid field');
+    this.name = 'InvalidFieldError';
+    this.message = `Field: '${field}' is not allowed for table: '${tableName}'`;
+    this.status = 400;
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, InvalidFieldError);
+    }
+  }
+}
