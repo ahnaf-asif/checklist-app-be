1c0e469e61f85f24866a7556986804eceb1a7faa
Mubasshir Chowdhury <md.mubasshir.chowdhury@gmail.com>
2024-09-23 05:28:29 +0600

    Update DbModel and UserModel with error handling and proper type

--- src/db/models/DbModel.ts
+++ src/db/models/DbModel.ts
@@ -1,57 +1,124 @@
+import { QueryResultRow } from 'pg';
+
 import { query } from '../';
+import Either from '../../utils/either';
+import { NoRecordFoundError, InvalidFieldError } from '../../utils/errors';
 
-export default class DbModel {
-  public tableName: string;
-  public primaryKey: string;
-  public fields: string[];
+export default class DbModel implements QueryResultRow {
+  public static tableName: string;
+  public static primaryKey: string;
+  public static fields: string[];
+  public id!: number;
 
-  constructor(tableName: string, primaryKey: string, fields: string[]) {
-    this.tableName = tableName;
-    this.primaryKey = tableName;
-    this.fields = fields;
-  }
+  constructor(_props: Record<string, any>) {}
 
-  public async find(id: number) {
-    return await query(`SELECT * FROM ${this.tableName} WHERE ${this.primaryKey} = ${id}`);
-  }
-
-  public async findAll() {
-    return `SELECT * FROM ${this.tableName}`;
-  }
-
-  public async create(data: Record<string, any>) {
-    const columns = Object.keys(data).join(', ');
-
-    for (const field of columns) {
-      if (!this.fields.includes(field)) {
-        throw new Error(`Field ${field} is not allowed`);
+  public static async find(id: number): Promise<Either<DbModel>> {
+    return Either.tryAsync<DbModel>(async () => {
+      const result = await query<DbModel>(
+        `SELECT * FROM ${this.tableName} WHERE ${this.primaryKey} = ${id}`
+      );
+      if (result.rowCount === 0) {
+        throw new NoRecordFoundError(this.tableName, id);
       }
-    }
-
-    const values = Object.values(data).join(', ');
-
-    return await query(`INSERT INTO ${this.tableName} (${columns}) VALUES (${values})`);
+      return result.rows[0];
+    });
   }
 
-  public async update(id: number, data: Record<string, any>) {
-    const keys = Object.keys(data);
-    const values = Object.values(data);
-    const updData = [];
+  public static async findAll(): Promise<Either<DbModel[]>> {
+    return Either.tryAsync<DbModel[]>(async () => {
+      const result = await query<DbModel>(`SELECT * FROM ${this.tableName}`);
+      return result.rows;
+    });
+  }
 
-    for (const field of keys) {
-      if (!this.fields.includes(field)) {
-        throw new Error(`Field ${field} is not allowed`);
+  public static async create(data: Record<string, any>): Promise<Either<DbModel>> {
+    return Either.tryAsync<DbModel>(async () => {
+      const columns = Object.keys(data).join(', ');
+      for (const field of Object.keys(data)) {
+        if (field === this.primaryKey) {
+          continue;
+        }
+        if (!this.fields.includes(field)) {
+          throw new InvalidFieldError(this.tableName, field);
+        }
       }
-    }
 
-    for (let i = 0; i < keys.length; i++) {
-      updData.push(`${keys[i]} = ${values[i]}`);
-    }
+      const values = Object.values(data)
+        .map((value) => {
+          if (typeof value === 'string') {
+            return `'${value}'`;
+          }
 
-    return await query(`UPDATE ${this.tableName} SET ${updData} WHERE ${this.primaryKey} = ${id}`);
+          return value;
+        })
+        .join(', ');
+
+      const result = await query<DbModel>(
+        `INSERT INTO ${this.tableName} (${columns}) VALUES (${values}) RETURNING *`
+      );
+      return result.rows[0];
+    });
   }
 
-  public async delete(id: number) {
-    return query(`DELETE FROM ${this.tableName} WHERE ${this.primaryKey} = ${id}`);
+  public static async update(id: number, data: Record<string, any>): Promise<Either<DbModel>> {
+    return Either.tryAsync<DbModel>(async () => {
+      const keys = Object.keys(data);
+      const values = Object.values(data);
+      const updData = [];
+
+      for (const field of keys) {
+        if (!this.fields.includes(field)) {
+          throw new InvalidFieldError(this.tableName, field);
+        }
+      }
+
+      for (let i = 0; i < keys.length; i++) {
+        let value = values[i];
+        if (typeof value === 'string') {
+          value = `'${value}'`;
+        }
+
+        updData.push(`${keys[i]} = ${value}`);
+      }
+
+      const result = await query<DbModel>(
+        `UPDATE ${this.tableName} SET ${updData} WHERE ${this.primaryKey} = ${id} RETURNING *`
+      );
+      if (result.rowCount === 0) {
+        throw new NoRecordFoundError(this.tableName, id);
+      }
+      return result.rows[0];
+    });
+  }
+
+  public async update(data: Record<string, any>): Promise<DbModel> {
+    const { val: _updated, err } = await (this.constructor as typeof DbModel).update(this.id, data);
+    if (err) {
+      throw err;
+    }
+
+    // update the instance with the new data
+    for (const key of Object.keys(data)) {
+      // @ts-ignore: Allow dynamic property assignment
+      this[key] = data[key];
+    }
+    return this;
+  }
+
+  public static async delete(id: number): Promise<Either<void>> {
+    return Either.tryAsync<void>(async () => {
+      const result = await query(`DELETE FROM ${this.tableName} WHERE ${this.primaryKey} = ${id}`);
+      if (result.rowCount === 0) {
+        throw new NoRecordFoundError(this.tableName, id);
+      }
+    });
+  }
+
+  public async delete(): Promise<void> {
+    try {
+      await (this.constructor as typeof DbModel).delete(this.id);
+    } catch (error) {
+      throw error;
+    }
   }
 }
--- src/db/models/UserModel.ts
+++ src/db/models/UserModel.ts
@@ -1,8 +1,79 @@
 import DbModel from './DbModel';
-export default class UserModel extends DbModel {
-  constructor() {
-    super('users', 'id', ['email', 'password', 'name']);
+import Either from '../../utils/either';
+
+type UserProps = {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  name: string;
+  created_at?: Date;
+  updated_at?: Date;
+};
+
+export default class User extends DbModel implements UserProps {
+  public static tableName = 'users';
+  public static primaryKey = 'id';
+  public static fields = ['username', 'email', 'password', 'name', 'created_at', 'updated_at'];
+
+  public id!: number;
+  public username!: string;
+  public email!: string;
+  public password!: string;
+  public name!: string;
+  public created_at!: Date;
+  public updated_at!: Date;
+
+  constructor(props: UserProps) {
+    super(props);
+    Object.assign(this, props);
   }
 
-  // additional methods if necessary.
+  public static async find(id: number): Promise<Either<User>> {
+    const { val: user, err } = await super.find(id);
+    if (err) {
+      return new Either<User>(undefined, err);
+    }
+    // @ts-ignore
+    return new Either<User>(new User(user as UserProps), err);
+  }
+
+  public static async findAll(): Promise<Either<User[]>> {
+    const { val: users, err } = await super.findAll();
+    if (err) {
+      return new Either<User[]>(undefined, err);
+    }
+    return new Either<User[]>(
+      // @ts-ignore
+      users!.map((user) => new User(user as UserProps)),
+      err
+    );
+  }
+
+  public static async create(
+    props: Omit<UserProps, 'id'> & { id?: number }
+  ): Promise<Either<User>> {
+    const { val: user, err } = await super.create(props);
+    if (err) {
+      return new Either<User>(undefined, err);
+    }
+    // @ts-ignore
+    return new Either<User>(new User(user as UserProps), err);
+  }
+
+  public static async update(
+    id: number,
+    props: Partial<Omit<UserProps, 'id'>>
+  ): Promise<Either<User>> {
+    const { val: user, err } = await super.update(id, props);
+    if (err) {
+      return new Either<User>(undefined, err);
+    }
+    // @ts-ignore
+    return new Either<User>(new User(user as UserProps), err);
+  }
+
+  public async update(props: Partial<Omit<UserProps, 'id'>>): Promise<User> {
+    return super.update(props as Record<string, any>) as Promise<User>;
+  }
 }
