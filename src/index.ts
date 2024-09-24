bf2c539d3bf286f9698ff163efc778b98d97723c
ahnaf-asif <ahnafshahriar92@gmail.com>
2024-09-25 02:14:17 +0600

    Add cors
    * Add cors package, and add cors related functionality so that the backend accepts requests from localhost

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as process from 'process';
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import apiRouter from './api';
 
@@ -9,9 +10,14 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 6969;
 
-// will remove/change them later
-
 app.use(express.json());
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
+);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server!!');
